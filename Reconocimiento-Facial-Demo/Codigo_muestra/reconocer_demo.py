3a7dca35c540fd0238b0dee7b87a17c141b41784
Marysa01 <161401484+Marysa01@users.noreply.github.com>
2026-05-29 20:23:38 -0700

    Delete Reconocimiento-Facial-Demo/Codigo_muestra/reconocer_demo.py

--- Reconocimiento-Facial-Demo/Codigo_muestra/reconocer_demo.py
+++
@@ -1,7 +0,0 @@
-id_predicho, distancia = face_recognizer.predict(rostro)
-
-if distancia < UMBRAL_EXISTENCIA:
-    nombre = imagePaths[id_predicho]
-    print(f"Acceso permitido: {nombre}")
-else:
-    print("Usuario desconocido")
